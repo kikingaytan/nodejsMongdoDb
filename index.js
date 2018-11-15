1803f86c62c031458e4a6cfe82680b5ce04f7f11
Enrique <enrique.diaz.gaytan@gmail.com>
2018-11-15 08:54:57 -0600

    adding the image field

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ const express = require('express');
 const app = express();
 
 mongoose.connect('mongodb://localhost:37017/soccer')
-  .then(() => console.log('Connected to MongoDB...'))
-  .catch(err => console.error('Could not connect to MongoDB...'));
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDB...'));
 
 app.use(express.json());
 app.use('/api/people', people);
 app.use('/api/teams', teams);
-
+//expone el folder que se usa para exponer los archivos que se guardan
+//image upload 
+//https://www.youtube.com/watch?v=srPXMt1Q0nY
+app.use(express.static('uploads'));
+//app.use('/uploads',express.static('uploads'));
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
--- routes/people.js
+++ routes/people.js
@@ -1,22 +1,58 @@
-const {getTeams,getTeamById,saveTeam,removeTeam,addPlayerOnTeam, 
-    addCoachOnTeam,addEventOnTeam,getPlayersByTeamId,getCoachesByTeamId,
-    getEventsByTeamId,savePerson,getPeople} = require('../service/teamsService.js');
+const {
+    getTeams,
+    getTeamById,
+    saveTeam,
+    removeTeam,
+    addPlayerOnTeam,
+    addCoachOnTeam,
+    addEventOnTeam,
+    getPlayersByTeamId,
+    getCoachesByTeamId,
+    getEventsByTeamId,
+    savePerson,
+    getPeople
+} = require('../service/teamsService.js');
 
 const express = require('express');
-const mongoose = require('mongoose');
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'c:\\uploads')
+    },
+    filename: function(req, file, cb) {
+        //cb(null,file.filename);
+        cb(null, new Date().toISOString() + file.originalname);
+    },
+});
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
+});
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
+
 const router = express.Router();
-router.get('/', async (req,res) =>{
+router.get('/', async(req, res) => {
     let people = await getPeople();
     res.send(people);
 
 });
 
-router.post('/', async (req, res) => {
+router.post('/', upload.single('photeImage'), async(req, res) => {
     //const { error } = validate(req.body); 
     //if (error) return res.status(400).send(error.details[0].message);
-  
+    console.log(req.file.path);
+    // fieldProductImage: req.file.path
     var person = await savePerson(req.body);
-    
+
     res.send(person);
-  });
+});
 module.exports = router;
